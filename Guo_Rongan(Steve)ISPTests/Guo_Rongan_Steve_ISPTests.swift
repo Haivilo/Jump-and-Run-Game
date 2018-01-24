fd95c59101745bc4c7469da1b7970e6c4b06b4af
Rongan Guo <ronganguo@Rongans-MacBook-Pro.local>
2018-01-23 21:58:33 -0500

    Initial Commit

---
+++ Guo_Rongan(Steve)ISPTests/Guo_Rongan_Steve_ISPTests.swift
@@ -0,0 +1,36 @@
+//
+//  Guo_Rongan_Steve_ISPTests.swift
+//  Guo_Rongan(Steve)ISPTests
+//
+//  Created by Rongan Guo on 2018-01-23.
+//  Copyright © 2018 Rongan Guo. All rights reserved.
+//
+
+import XCTest
+@testable import Guo_Rongan_Steve_ISP
+
+class Guo_Rongan_Steve_ISPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
